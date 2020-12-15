aae47174bba9526724f6a795e7cb796f92377690
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 19:37:28 -0600

    add initial routes with quiz page and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.scss';
+import './styles/App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import QuizPage from './pages/QuizPage';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Router>
+      <Route exact path='/'>
+        <HomePage />
+      </Route>
+      <Route exact path='/quiz'>
+        <QuizPage />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
