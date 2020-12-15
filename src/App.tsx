d81edc8a66cd5cbe5aba5c8dbd3a65e4ed23d92e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 19:48:09 -0600

    setup initial navbar with home and quiz links

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './styles/App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import QuizPage from './pages/QuizPage';
+import Nav from './components/nav/Nav.component';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Route exact path='/'>
         <HomePage />
       </Route>
---
+++ src/components/nav/Nav.component.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.styles.scss';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className='nav-items'>
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <Link to='/quiz'>
+          <li>Quiz</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
