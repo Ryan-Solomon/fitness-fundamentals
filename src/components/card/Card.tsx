6c4d3d2ecefa489411b65a91b88fcef75760b1ad
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 20:16:13 -0600

    add card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -5,7 +5,9 @@ const Card = () => {
   return (
     <div className='l-container'>
       <div className='b-game-card'>
-        <div className='b-game-card__cover'></div>
+        <div className='b-game-card__cover'>
+          <h1>Beginner</h1>
+        </div>
       </div>
     </div>
   );
