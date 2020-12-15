78e4c105cb1d09327ccf16e0211ec6fc51f3120d
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 20:40:36 -0600

    add bg gradient

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Card from './../components/card/Card';
 
 const HomePage = () => {
-  return (
-    <div>
-      <Card />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default HomePage;
