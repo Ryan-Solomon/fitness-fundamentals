d28a529d529b54da6f968b3210c5f9055be456df
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-15 18:16:02 -0600

    render out question, incorrect answer, and other question data based on questionIdx

--- src/components/quiz/QuizQuestions.tsx
+++ src/components/quiz/QuizQuestions.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { useQuizContext } from '../../context/quizContext';
 
 const QuizQuestions = () => {
+  const [questionIdx, setQuestionIdx] = useState(0);
   const { id } = useParams<{ id: string }>();
-
   const { data } = useQuizContext();
-  const categoryData = data.filter((d) => d.category.toLowerCase() === id);
-  console.log(categoryData);
+  const categoryData = data.filter((d) => d.category.toLowerCase() === id)[0];
+  const currentQuestionData = categoryData.questions[questionIdx];
+  const {
+    question,
+    incorrectAnswers,
+    correctAnswer,
+    answeredCorrectly,
+  } = currentQuestionData;
 
-  return <div>Quiz Questions</div>;
+  return <div>{question}</div>;
 };
 
 export default QuizQuestions;
