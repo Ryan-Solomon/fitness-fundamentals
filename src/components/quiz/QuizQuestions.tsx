0649790c0b455342450c570d99d0128e7c4249f5
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-15 19:53:00 -0600

    add question card

--- src/components/quiz/QuizQuestions.tsx
+++ src/components/quiz/QuizQuestions.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useQuizContext } from '../../context/quizContext';
+import { BsQuestionSquareFill } from 'react-icons/bs';
+import './QuizQuestions.scss';
 
 const QuizQuestions = () => {
   const [questionIdx, setQuestionIdx] = useState(0);
@@ -15,7 +17,27 @@ const QuizQuestions = () => {
     answeredCorrectly,
   } = currentQuestionData;
 
-  return <div>{question}</div>;
+  return (
+    <div className='container'>
+      <div className='card'>
+        <div className='face face1'>
+          <div className='content'>
+            <BsQuestionSquareFill size={100} />
+            <h3>Question {questionIdx + 1}</h3>
+          </div>
+        </div>
+        <div className='face face2'>
+          <div className='content'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum
+              cumque minus iste veritatis provident at.
+            </p>
+            <a href='#'>Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuizQuestions;
