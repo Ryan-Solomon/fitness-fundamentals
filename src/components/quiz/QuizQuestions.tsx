e4b50d51ca315ce6e02261a6759f1591aa90a82f
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-15 18:10:53 -0600

    get relevant data based on params id

--- src/components/quiz/QuizQuestions.tsx
+++ src/components/quiz/QuizQuestions.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { useQuizContext } from '../../context/quizContext';
 
 const QuizQuestions = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { data } = useQuizContext();
+  const categoryData = data.filter((d) => d.category.toLowerCase() === id);
+  console.log(categoryData);
+
   return <div>Quiz Questions</div>;
 };
 
