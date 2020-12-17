c9f3f07182625cec59134a92e3cf333f6f4f965a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-16 18:13:39 -0600

    add sidebar layout

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const SideBar = () => {
-  return <div>This Is A SideBar!</div>;
+  return (
+    <div className='sidebar-container'>
+      <h1>Lessons</h1>
+      <ul>
+        <li>Lesson 1 - Training Principles Introduction</li>
+        <li>Lesson 2 - Specificity</li>
+        <li>Lesson 3 - Progressive Overload</li>
+        <li>Lesson 4 - Fatigue Management</li>
+        <li>Lesson 5 - Training Variables Introduction</li>
+        <li>Lesson 6 - Volume</li>
+        <li>Lesson 7 - Frequency</li>
+        <li>Lesson 9 - Relative Intensity</li>
+        <li>Lesson 8 - Absolute Intensity</li>
+        <li>Lesson 10 - </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
