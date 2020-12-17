edc8b1a11d56330857b81b036c46a4730d0b8465
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-17 17:29:25 -0600

    add layout for fundamentals component and initial grid

--- src/components/fundamentals/Fundamentals.tsx
+++ src/components/fundamentals/Fundamentals.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import SideBar from '../side-bar/SideBar';
+import './Fundamentals.styles.scss';
 
 const Fundamentals = () => {
   return (
-    <>
+    <div className='fundamentals-container'>
       <SideBar />
-      <h1> Fundamentals</h1>
-    </>
+      <section className='fundamentals-content'>
+        <h1>Lesson 1</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          molestias, consectetur iusto illo non impedit vel voluptate? Aperiam
+          consequuntur, dignissimos unde, ab harum, quisquam quia commodi libero
+          saepe quasi placeat? Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Labore molestias, consectetur iusto illo non impedit
+          vel voluptate? Aperiam consequuntur, dignissimos unde, ab harum,
+          quisquam quia commodi libero saepe quasi placeat? Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Labore molestias, consectetur
+          iusto illo non impedit vel voluptate? Aperiam consequuntur,
+          dignissimos unde, ab harum, quisquam quia commodi libero saepe quasi
+          placeat?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          molestias, consectetur iusto illo non impedit vel voluptate? Aperiam
+          consequuntur, dignissimos unde, ab harum, quisquam quia commodi libero
+          saepe quasi placeat? Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Labore molestias, consectetur iusto illo non impedit
+          vel voluptate? Aperiam consequuntur, dignissimos unde, ab harum,
+          quisquam quia commodi libero saepe quasi placeat? Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Labore molestias, consectetur
+          iusto illo non impedit vel voluptate? Aperiam consequuntur,
+          dignissimos unde, ab harum, quisquam quia commodi libero saepe quasi
+          placeat?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          molestias, consectetur iusto illo non impedit vel voluptate? Aperiam
+          consequuntur, dignissimos unde, ab harum, quisquam quia commodi libero
+          saepe quasi placeat? Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Labore molestias, consectetur iusto illo non impedit
+          vel voluptate? Aperiam consequuntur, dignissimos unde, ab harum,
+          quisquam quia commodi libero saepe quasi placeat? Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Labore molestias, consectetur
+          iusto illo non impedit vel voluptate? Aperiam consequuntur,
+          dignissimos unde, ab harum, quisquam quia commodi libero saepe quasi
+          placeat?
+        </p>
+      </section>
+    </div>
   );
 };
 
