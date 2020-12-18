7ba1f155651eb0a15bef22934e9e5a8d212a3aa9
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-17 19:45:11 -0600

    add folding and unfolding of sidebar

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
 import './SideBar.styles.scss';
+import { RiMenuUnfoldFill, RiMenuFoldFill } from 'react-icons/ri';
 
 const SideBar = () => {
-  const [show, setShow] = React.useState(true);
+  const [show, setShow] = React.useState(false);
 
-  if (!show) return null;
+  //   if (!show)
+  //     return (
+  //       <div className='sidebar-open' onClick={() => setShow(true)}>
+  //         <RiMenuUnfoldFill size={100} />
+  //       </div>
+  //     );
 
   return (
     <>
-      <nav className='container'>
-        <button onClick={() => setShow(false)}>X</button>
-        <div className='link'>
-          <div className='text'>Lessons</div>
-        </div>
-        <div className='link'>
-          <div className='text'>1. Projects</div>
-        </div>
-        <div className='link'>
-          <div className='text'>2. Art</div>
-        </div>
-        <div className='link'>
-          <div className='text'>3. Social</div>
-        </div>
-        <div className='link'>
-          <div className='text'>4. Setup</div>
-        </div>
-        <div className='link'>
-          <div className='text'>5. Help</div>
-        </div>
-      </nav>
+      {' '}
+      {show ? (
+        <nav className='container'>
+          <button onClick={() => setShow(false)} className='close-sidebar'>
+            <RiMenuFoldFill size={30} />
+          </button>
+          <div className='link'>
+            <div className='text'>Lessons</div>
+          </div>
+          <div className='link'>
+            <div className='text'>1. Projects</div>
+          </div>
+          <div className='link'>
+            <div className='text'>2. Art</div>
+          </div>
+          <div className='link'>
+            <div className='text'>3. Social</div>
+          </div>
+          <div className='link'>
+            <div className='text'>4. Setup</div>
+          </div>
+          <div className='link'>
+            <div className='text'>5. Help</div>
+          </div>
+        </nav>
+      ) : (
+        <nav className='container'>
+          <button className='open-sidebar' onClick={() => setShow(true)}>
+            <RiMenuUnfoldFill size={30} />
+          </button>
+        </nav>
+      )}
     </>
   );
 };
